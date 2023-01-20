724d8da9ba04a196247ac447a60f20b37692d331
umar7alabi <78322471+umar7alabi@users.noreply.github.com>
2023-01-20 04:14:02 +0300

    1/20 system configuration

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -45,9 +45,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "04ff2868ccfb0e23abadd881a93d05c3",
-"/": "04ff2868ccfb0e23abadd881a93d05c3",
-"main.dart.js": "9e30e649cd40231a0689f0e3cc1342e8",
+"index.html": "c3be784c5b3600ca1633c77d836def3b",
+"/": "c3be784c5b3600ca1633c77d836def3b",
+"main.dart.js": "e8ac67812889418ae1a439a1ca1b39f7",
 "manifest.json": "24c2ea01bf6d4d4f996312bcdb6f0d3d",
 "version.json": "22b2282f384198cd27b7b5fd0ab8aaa3"
 };
