23d15e9eb038157f607b6ec553c588221177cfc7
umar7alabi <78322471+umar7alabi@users.noreply.github.com>
2023-05-03 01:27:05 +0300

    privacy

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -52,8 +52,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "ad065f2d9556c01b4593a34eaa4f1a81",
-"/": "ad065f2d9556c01b4593a34eaa4f1a81",
+"index.html": "f17a0c454fda2aaee624a345bee8c08e",
+"/": "f17a0c454fda2aaee624a345bee8c08e",
 "main.dart.js": "9c416d0927306e1a5e53bbbb3c55c19b",
 "manifest.json": "24c2ea01bf6d4d4f996312bcdb6f0d3d",
 "version.json": "22b2282f384198cd27b7b5fd0ab8aaa3"
